b506000dc899804dbb9410e5133b8d1fd8e92890
Harshdeep Singh <harshdeepsingh13@gmail.com>
2018-01-19 23:25:35 +0530

    Sign up Implementation in process

--- js/node/nodeJSBack.js
+++ js/node/nodeJSBack.js
@@ -3,11 +3,16 @@ var mongoClient = require('mongodb').MongoClient;
 var formidable = require('formidable');
 var url = require('url');
 var fileServer = require('fs');
+var util = require('util');
 
 http.createServer(function(req,res){
     // var reqUrl = url.parse(req.url,true);
     var mongoUrl = 'mongodb://localhost:27017/';
-        if(req.url=='/checkLogin'){
+    var q = url.parse(req.url,true);
+    console.log(q);
+    console.log("pathname: " + q.pathname);
+        if(q.pathname=='/checkLogin'){
+            console.log('checkLogin');
             var form = new formidable.IncomingForm();
             form.parse(req,function(err,fields,files){
                var username = fields.username;
@@ -28,25 +33,29 @@ http.createServer(function(req,res){
                });
             });
         }
-        if(req.url == '/checkSignUp'){
+        else if(q.pathname == '/checkSignUp'){
+            console.log('checkSignUp');
             var form = new formidable.IncomingForm();
             form.parse(req,function(err,fields,files){
+                console.log(util.inspect({fields:fields, files:files}));
                 var username = fields.username;
 
                 mongoClient.connect(mongoUrl,function(err,db){
                     if(err) throw err;
                     var dbo = db.db('clients');
                     dbo.collection('myUsers').find({username:username}).toArray(function (mongoError, objects) {
+
                         if(mongoError) throw mongoError;
+                        console.log("username: " + username);
                         if(objects.length==0)
                         {
                             var name = fields.name;
                             var password=fields.password;
+                            console.log("name: " + name + "username:" + username + "password: " + password);
                             dbo.collection('myUsers').insertOne({name:name,username:username,password:password},function (err,objects) {
-                                res.writeHead(200,{'Content-Type':'text/html'});
-                                res.write('you can now log in!');
-                                res.redirect(200,'login.html');
+                                res.writeHead(301,{Location:'login.html'});
                                 res.end();
+                                db.close();
                             })
                         }
                         else
@@ -64,4 +73,8 @@ http.createServer(function(req,res){
                 });
             })
         }
-});
+        else {
+            res.writeHead(404,{'Content-Type':'text/html'});
+            res.end('page not found its me');
+        }
+}).listen(8080);
