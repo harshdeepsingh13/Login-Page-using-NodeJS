87a22f0187a75426c16fde270703621d26ad5b1d
Harshdeep Singh <harshdeepsingh13@gmail.com>
2018-01-21 21:15:14 +0530

    Login facility added, sign up is added, database connectivity with mongoDB for verifying and storing the records of users.

--- nodeJSBack.js
+++ nodeJSBack.js
@@ -4,13 +4,14 @@ var formidable = require('formidable');
 var url = require('url');
 var fileServer = require('fs');
 var util = require('util');
+var obj;
 
 http.createServer(function(req,res){
     // var reqUrl = url.parse(req.url,true);
     var mongoUrl = 'mongodb://localhost:27017/';
     var q = url.parse(req.url,true);
-    console.log(q);
-    console.log("pathname: " + q.pathname);
+    // console.log(q);
+    // console.log("pathname: " + q.pathname);
         if(q.pathname=='/checkLogin'){
             console.log('checkLogin');
             var form = new formidable.IncomingForm();
@@ -21,13 +22,14 @@ http.createServer(function(req,res){
                mongoClient.connect(mongoUrl,function(err,db){
                    if(err) throw err;
                    var dbo = db.db('clients');
-                   var obj={username:username, password:password};
+                   obj={username:username, password:password};
                    dbo.collection('myUsers').find(obj).toArray(function(mongoErr,result){
                        if(mongoErr)
                        {
                            throw mongoErr;
                        }
-                       res.redirect(200,'/about');
+                       res.writeHead(301,{Location:'/about'});
+                       res.end();
                        db.close();
                    });
                });
@@ -37,7 +39,7 @@ http.createServer(function(req,res){
             console.log('checkSignUp');
             var form = new formidable.IncomingForm();
             form.parse(req,function(err,fields,files){
-                console.log(util.inspect({fields:fields, files:files}));
+                util.inspect({fields:fields, files:files});
                 var username = fields.username;
 
                 mongoClient.connect(mongoUrl,function(err,db){
@@ -53,24 +55,64 @@ http.createServer(function(req,res){
                             var password=fields.password;
                             console.log("name: " + name + "username:" + username + "password: " + password);
                             dbo.collection('myUsers').insertOne({name:name,username:username,password:password},function (err,objects) {
-                                res.writeHead(301,{Location:'login.html'});
-                                res.end();
+
+                                /*res.writeHead(301,{Location:'login.html'});
+                                res.end();*/
+                                res.writeHead(200,{'Content-Type':'text/html'});
+                                fileServer.readFile('login.html',function (err,data) {
+                                    if(err) throw err;
+                                    res.write(data);
+                                    res.end();
+                                })
                                 db.close();
                             })
                         }
                         else
                         {
+                            console.log('else');
                             db.close();
                             res.writeHead(200,{'Content-Type':'text/html'});
-                            res.write('username already used. Retry');
-                            fileServer.readFile('signUp.html',function(err,data){
+                            res.write("<script>if(confirm('hey')){document.location = 'http://localhost:8080/errorSignUp'}</script>");
+                            res.end();
+
+
+                            /*fileServer.readFile('signUp.html',function(err,data){
                                 res.write(data);
                                 res.end();
-                            });
+                            });*/
 
                         }
                     })
                 });
+            })
+        }
+        else if(q.pathname == '/about'){
+            console.log('about');
+            mongoClient.connect(mongoUrl,function (err,db) {
+                if(err) throw err;
+                var dbo = db.db('clients');
+                dbo.collection('myUsers').find(obj).toArray(function (mongoError, objects) {
+                    if(mongoError) throw mongoError;
+                    util.inspect({objects:objects});
+                    res.writeHead(200,{'Content-Type':'text/html'});
+                    // objects.forEach(function (value) { console.log(value.valueOf()) });
+                    var name = objects[0].name;
+
+                    res.write('Hey, ' + name + '<br> How are you?');
+                    res.end();
+                });
+            });
+        }
+        else if(q.pathname == '/errorSignUp'){
+            console.log('errorSignUp');
+            // res.writeHead(301,{Location:'signUp.html'});
+            // res.end();
+            res.writeHead(200,{'Content-Type':'text/html'});
+            fileServer.readFile('signUp.html',function (err,data) {
+                if(err) throw err;
+                res.write(data);
+                res.end();
+
             })
         }
         else {
